779d153403a16ac7096719057729b88d4590d83f
FreddyJr27 <freddyjr200427@gmail.com>
2024-06-14 23:28:17 -0400

    se avanzo con el proyecto

--- Main.py
+++ Main.py
@@ -1,14 +1,14 @@
 class Proyectos:
-    def __init__(self, id,nombre,descripcion,fecha_inicio,fecha_fin,estado,empresa,gerente,equipo):
-        self.id = id
-        self.nombre = nombre
-        self.descripcion = descripcion
-        self.fecha_inicio = fecha_inicio
-        self.fecha_fin = fecha_fin
-        self.estado = estado
-        self.empresa = empresa
-        self.gerente = gerente
-        self.equipo = equipo
+    def __init__(self):
+        self.id = input(str("ingrese el id del proyecto: "))
+        self.nombre = input(str("ingrese el nombre del proyecto: "))
+        self.descripcion = input(str("ingrese la descripcion del proyecto: "))
+        self.fecha_inicio = input(str("ingrese la fecha de inicio del proyecto: "))
+        self.fecha_fin = input(str("ingrese la fecha de fin del proyecto: "))
+        self.estado = input(str("ingrese el estado del proyecto: "))
+        self.empresa = input(str("ingrese la empresa del proyecto: "))
+        self.gerente = input(str("ingrese el gerente del proyecto: "))
+        self.equipo = input(str("ingrese el equipo del proyecto: "))
         listilla = []
     def __str__(self):
         return f'{self.id} {self.nombre} {self.descripcion} {self.fecha_inicio} {self.fecha_fin} {self.estado} {self.empresa} {self.gerente} {self.equipo}'
@@ -24,5 +24,30 @@ class Proyectos:
         listilla.append(self.gerente)
         listilla.append(self.equipo)
         return listilla
-proyecto1 = Proyectos(1,"Proyecto 1","Proyecto de prueba","2021-01-01","2021-12-31","Activo","Empresa 1","Gerente 1","Equipo 1")
-print(proyecto1.listaentre())
+class listagrande:
+    def __ini__(self):
+        
+        self.lista = []
+    def agregar(self):
+       while True:
+
+            respuesta = input("desea agregar un proyecto? (s/n): ")
+            if respuesta.lower() == 's':
+                proyecto = Proyectos()
+                self.lista.append(proyecto.listaentre())
+
+            elif respuesta.lower() == 'n':
+                print("Gracias por usar el programa")
+                break
+                
+            else:
+                print("Por favor, ingrese 's' para agregar un nuevo proyecto o 'n' para terminar.") 
+
+    def mostrar(self):
+        for proyecto in self.lista:
+            print(proyecto)
+
+lista = listagrande()
+lista.agregar()
+lista.mostrar()
+
