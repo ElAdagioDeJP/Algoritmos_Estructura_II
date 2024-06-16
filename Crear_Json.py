67f36b09871b0b927612be1f1fb913fc5ad9c09c
ElAdagioDeJP <129967733+ElAdagioDeJP@users.noreply.github.com>
2024-06-15 22:45:28 -0400

    Killing me to make a json file, help

--- Crear_Json.py
+++ Crear_Json.py
@@ -1,4 +1,17 @@
 import json
 
+def Guardar_json(proyecto):
+    try:
+        with open('data.json', 'w') as archivo:
+            json.dump(proyecto, archivo, indent=4)
+    except FileNotFoundError:
+        print("No se encontro el archivo deah 😈")
+        
+"""
 data = {}
-data['tareas'] = []
+data['Empresa'] = []
+data['Empresa'].append({
+    "Proyecto": [],
+    
+    
+})"""
