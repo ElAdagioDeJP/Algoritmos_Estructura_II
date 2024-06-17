229ad00a0831f02849d61675a3eb6f56b8f4d11f
abelipe <157862231+abelipe@users.noreply.github.com>
2024-06-16 20:53:58 -0400

    update clase listas
    listas enlazadas y nodo

--- ejemplo.py
+++ ejemplo.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import json
+
 class Proyecto:    
     def __init__(self, id, nombre, descripcion, fecha_inicio, fecha_vencimiento, estado, empresa, gerente, equipo):
         self.id = id
@@ -86,3 +87,141 @@ def cargar_datos_desde_json(nombre_archivo):
             proyecto.calcular_p()
             proyectos.append(proyecto)
     return proyectos
+
+
+
+class Nodo:
+    def __init__(self, valor=None):
+        self.valor = valor
+        self.siguiente = None
+        
+class ListaEnlazada:
+    def __init__(self):
+        self.cabeza = None
+        self.longitud = 0
+    def __len__(self):
+        return self.longitud
+    def __iter__(self):
+        actual = self.cabeza
+        while actual:
+            yield actual.valor
+            actual = actual.siguiente
+            
+    def agregar(self, valor):
+        nuevo_nodo = Nodo(valor)
+        if self.cabeza is None:
+            self.cabeza = nuevo_nodo
+        else:
+            actual = self.cabeza
+            while actual.siguiente:
+                actual = actual.siguiente
+            actual.siguiente = nuevo_nodo
+        self.longitud += 1
+        
+    def eliminar(self, valor):
+        if self.cabeza is None:
+            return False
+        if self.cabeza.valor == valor:
+            self.cabeza = self.cabeza.siguiente
+            self.longitud -= 1
+            return True
+        actual = self.cabeza
+        while actual.siguiente:
+            if actual.siguiente.valor == valor:
+                actual.siguiente = actual.siguiente.siguiente
+                self.longitud -= 1
+                return True
+            actual = actual.siguiente
+        return False
+    
+    def insertar(self, indice, valor):
+        if indice < 0 or indice > self.longitud:
+            raise IndexError("Índice fuera de rango")
+        nuevo_nodo = Nodo(valor)
+        if indice == 0:
+            nuevo_nodo.siguiente = self.cabeza
+            self.cabeza = nuevo_nodo
+        else:
+            actual = self.cabeza
+            for i in range(indice - 1):
+                actual = actual.siguiente
+            nuevo_nodo.siguiente = actual.siguiente
+            actual.siguiente = nuevo_nodo
+        self.longitud += 1
+        
+    def obtener(self, indice):
+        if indice < 0 or indice >= self.longitud:
+            raise IndexError("Índice fuera de rango")
+        actual = self.cabeza
+        for i in range(indice):
+            actual = actual.siguiente
+        return actual.valor
+    
+    def index(self, valor):
+        actual = self.cabeza
+        indice = 0
+        while actual:
+            if actual.valor == valor:
+                return indice
+            actual = actual.siguiente
+            indice += 1
+        raise ValueError("{} no está en la lista".format(valor))
+    
+    def pop(self, indice=None):
+        if indice is None:
+            indice = self.longitud - 1
+        if indice < 0 or indice >= self.longitud:
+            raise IndexError("Índice fuera de rango")
+        if indice == 0:
+            varsalor = self.cabeza.valor
+            self.cabeza = self.cabeza.siguiente
+            self.longitud -= 1
+            return valor
+        actual = self.cabeza
+        for i in range(indice - 1):
+            actual = actual.siguiente
+        valor = actual.siguiente.valor
+        actual.siguiente = actual.siguiente.siguiente
+        self.longitud -= 1
+        return valor
+    
+def filtrar_tareas_por_rango(self, proyectos, fecha_inicio, fecha_fin):
+    tareas_filtradas = []
+    for proyecto in proyectos:
+        for tarea in proyecto.tareas:
+            if fecha_inicio <= tarea.fecha_inicio <= fecha_fin:
+                tareas_filtradas.append(tarea)
+    return tareas_filtradas
+    
+def filtrar_proyectos_por_rango(self, proyectos, fecha_inicio, fecha_fin):
+    proyectos_f = []
+    for proyecto in proyectos:
+        if fecha_inicio <= proyecto.fecha_inicio <= fecha_fin:
+            proyectos_f.append(proyecto)
+    return proyectos_f
+
+listoca = ListaEnlazada()
+ruta_archivo = "./datos_prueba.json"
+proyectox = cargar_datos_desde_json(ruta_archivo)
+for proyectos in proyectox:
+    print(proyectos.nombre)
+    for tarea in proyectos.tareas:
+        print(tarea.nombre)
+
+"""
+# Ejemplo de uso:
+
+# Creamos algunos objetos de la clase Carro
+carro1 = Carro("Toyota", "Corolla")
+carro2 = Carro("Honda", "Accord")
+carro3 = Carro("Ford", "Mustang")
+carro4 = Carro("Chevrolet", "Camaro")
+
+# Creamos la lista enlazada y agregamos algunos carros
+autos = ListaEnlazada()
+autos.agregar(carro1)
+autos.agregar(carro2)
+autos.agregar(carro3)
+
+lista = list(autos)
+"""
