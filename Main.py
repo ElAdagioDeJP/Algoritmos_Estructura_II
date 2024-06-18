75758b7aacda23899136ebf72d3eec94ef83effc
ElAdagioDeJP <129967733+ElAdagioDeJP@users.noreply.github.com>
2024-06-18 14:45:02 -0400

    Freddy is a bald guy who has to learn to program and change an address.

--- Main.py
+++ Main.py
@@ -654,6 +654,30 @@ class modificar:
             print("\t",y.fecha_inicio.date())
             print("\t",y.fecha_vencimiento.date())
             print("\t",y.estado)
+    def agrega_cola(self):
+
+        idt = input("Ingrese el id de la tarea: ")
+        nombret = input("Ingrese el nombre de la tarea: ")
+        empresa_clientet = input("Ingrese la empresa del cliente: ")
+        descripciont = input("Ingrese la descripcion de la tarea: ")
+        fecha_iniciot = datetime.strptime(input("Ingrese la fecha de inicio de la tarea: "), "%Y-%m-%d")
+        fecha_vencimientot = datetime.strptime(input("Ingrese la fecha de vencimiento de la tarea: "), "%Y-%m-%d")
+        estadot = input("Ingrese el estado de la tarea: ")
+        porcentajet = int(input("Ingrese el porcentaje de la tarea: "))
+        tareito = Tarea(idt, nombret, empresa_clientet, descripciont, fecha_iniciot, fecha_vencimientot, estadot, porcentajet)
+        j = int(input("Cuantas subtareas desea agregar: "))
+
+        while j !=0:
+
+            ids = input("Ingrese el id de la subtarea: ")
+            nombres = input("Ingrese el nombre de la subtarea: ")
+            descripcions = input("Ingrese la descripcion de la subtarea: ")
+            estados = input("Ingrese el estado de la subtarea: ")
+            subtareito = Subtarea(ids, nombres, descripcions, estados)
+            tareito.agregar_subtarea(subtareito)
+            j -= 1
+
+        self.cola.agregar(tareito)
 
 proyecto = modificar()
 #proyecto.agregar_huevonadas()
@@ -662,9 +686,9 @@ proyecto = modificar()
 #proyecto.eliminar_pila()
 #proyecto.recorre_pila()
 proyecto.ordenar_tareas_colas()
-proyecto.eliminar_cola()
+#proyecto.eliminar_cola()
+proyecto.agrega_cola()
 proyecto.recorre_cola()
-
 
 
 """
